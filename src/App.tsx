4cb4ae416dbfa5d44e06d5195384ecb80fbcff93
Arthur Biasibetti Farias <20212GR.ADS0006@gravatai.ifsul.edu.br>
2023-03-30 21:17:54 -0300

    começando testes com upload do csv

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { getDemo } from './api/demo'
+
+import style from './App.module.scss'
+import HttpClient from './config/axios'
+
+const uploadFile = (file: File) => {
+  const formData = new FormData();
+  formData.append(file.name, file)
+  console.log(formData)
+  HttpClient.api.post('/file', formData);
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    getDemo().then((data) => {console.log(data)})
+  }, [])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className={style.App}>
+        <input type='file' accept='.csv' formEncType="multipart/form-data" onChange={
+          (e) => {console.log(e.target.files && e.target.files[0])
+         if(e.target.files) uploadFile(e.target.files[0])}
+        }/>
     </div>
   )
 }
---
+++ src/config/axios.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const defaultHeaders = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': '*',
+};
+
+const axiosConfig = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}`,
+  timeout: 30000,
+  headers: defaultHeaders,
+});
+
+console.log(import.meta.env.VITE_API_URL)
+
+class HttpClient {
+  static api = axiosConfig;
+}
+
+export default HttpClient;
