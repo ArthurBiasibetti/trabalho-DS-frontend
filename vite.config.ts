ced7410438c923fbd66b2e9e524c9a20c3bb88fe
Arthur Asch <arthurlohransilva@gmail.com>
2023-05-17 20:52:21 -0300

    nao ironicamente estou coringando

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
-import { defineConfig } from 'vite';import react from '@vitejs/plugin-react';import mkcert from 'vite-plugin-mkcert';// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 export default defineConfig({
   plugins: [react(), basicSsl()],
   server: {
