8356a4e0697d1dfaf508946737e5ea7b9ddd5464
Arthurbiasibetti <arthurbiasibettifarias@gmail.com>
2023-07-14 15:26:12 -0300

    feat: tabela de exemplo do inventario

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,42 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-import style from './styles.module.scss'
+interface TableProps {
+  data: any[];
+  page?: number;
+  pageLength?: number;
+}
 
-export const Table: React.FC<{data: any[], fields: any[], page?: number, pageLength?: number}> = ({fields, data, page = 1, pageLength = data.length}) => {
+import style from './styles.module.scss';
+
+export const Table: React.FC<TableProps> = ({
+  data,
+  page = 1,
+  pageLength = data.length,
+}) => {
+  const fields = Object.keys(data[0]);
+
+  console.log(fields);
+
   return (
     <div>
       <table className={style.table}>
         <thead>
           <tr>
-            {fields.map((field, index) =>  <th>{field}</th> )}
+            {fields.map((field, index) => (
+              <th key={index}>{field}</th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          { data.map((data, index ) => pageLength * page >= index && pageLength * (page -1) <= index ? (
-            <tr>
-                <td>{data[fields[0]]}</td>
-                <td>{data[fields[1]]}</td>
-                <td>{data[fields[2]]}</td>
-                <td>{data[fields[3]]}</td>
-                <td>{data[fields[4]]}</td>
-                <td>{data[fields[5]]}</td>
-                <td>{data[fields[6]]}</td>
-                <td>{data[fields[7]]}</td>
-                <td>{data[fields[8]]}</td>
-                <td>{data[fields[9]]}</td>
-                <td>{data[fields[10]]}</td>
-                <td>{data[fields[11]]}</td>
-                <td>{data[fields[12]]}</td>
-                <td>{data[fields[13]]}</td>
-                <td>{data[fields[14]]}</td>
-                <td>{data[fields[15]]}</td>
-                <td>{data[fields[16]]}</td>
-                <td>{data[fields[17]]}</td>
-              </tr>
-            ): null)
-          }
-          </tbody>
+          {data.map((dataItem, index) => (
+            <tr key={index}>
+              {fields.map((field, index) => (
+                <td>{dataItem[field] || ''}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
--- src/pages/InvList/index.tsx
+++ src/pages/InvList/index.tsx
@@ -7,6 +7,7 @@ import { Container } from '../../components/Container';
 import styles from './styles.module.scss';
 import { Menu } from '../../components/Menu';
 import { Button } from '../../components/Button';
+import { Table } from '../../components/Table';
 
 export const InvListPage: React.FC = () => {
   return (
@@ -17,7 +18,37 @@ export const InvListPage: React.FC = () => {
         <p>Lista Inventário</p>
       </div>
 
-      <div className={styles['inventories-content']}></div>
+      <div className={styles['inventories-content']}>
+        <Table
+          data={[
+            {
+              '#': 1,
+              item: 'Computador LG 9283921',
+              codigo: '1274515183',
+            },
+            {
+              '#': 2,
+              item: 'Computador LG 9283921',
+              codigo: '1223145151',
+            },
+            {
+              '#': 31,
+              item: 'Computador LG 9283921',
+              codigo: '1274516651',
+            },
+            {
+              '#': 67,
+              item: 'Computador LG 9283921',
+              codigo: '1274512422',
+            },
+            {
+              '#': 4,
+              item: 'Computador LG 9283921',
+              codigo: '1274090882',
+            },
+          ]}
+        />
+      </div>
     </Container>
   );
 };
